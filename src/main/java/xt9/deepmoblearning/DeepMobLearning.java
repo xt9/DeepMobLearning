001425b83b7753a9583c68061f2c3bc9f4c43548
xt9 <niklas_andersson90@hotmail.com>
2017-06-08 05:54:57 +0200

    Use the gradle text replace for version in the main class.

--- src/main/java/xt9/deepmoblearning/DeepMobLearning.java
+++ src/main/java/xt9/deepmoblearning/DeepMobLearning.java
@@ -4,11 +4,11 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = DeepMobLearning.MODID, version = DeepMobLearning.VERSION)
+@Mod(modid = DeepMobLearning.MODID, version = DeepMobLearning.VERSION, updateJSON = "https://github.com/xt9/DeepMobLearner/blob/master/update.json")
 public class DeepMobLearning
 {
     public static final String MODID = "deepmoblearning";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "${version}";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
